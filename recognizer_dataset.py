7765f3b194267b6c14b8a4327aab4d0caabe513c
salgue441 <carlossalguero441@gmail.com>
2022-12-03 13:22:02 -0600

    Code for the dataset

---
+++ recognizer_dataset.py
@@ -0,0 +1,45 @@
+from re import X
+import keras
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+# Splitting the data between train and test sets
+print("---- Data ----")
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(x_train.shape, y_train.shape)
+
+# Reshaping the X data
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
+
+# Converting class vectors to binary matrices
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+print("\n---- Reshapes ----")
+print(f'x_train shape: {x_train.shape}')
+print(f'{x_train.shape[0]}, train samples')
+print(f'{x_test.shape[0]}, test samples')
+
+# Constants
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(5, 5), activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
